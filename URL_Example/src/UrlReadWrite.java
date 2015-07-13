93714e5df4038ea5853c3e7194ef855deb066d33
Lakshmipriya Sivakumar <lpriya.sivakumar@gmail.com>
2015-07-13 06:20:02 -0400

    Updated the string match skipping Title tags. Refined the logic to match upper and lower case of string.

--- URL_Example/src/UrlReadWrite.java
+++ URL_Example/src/UrlReadWrite.java
@@ -4,27 +4,43 @@ import java.io.*;
 public class UrlReadWrite {
 
 	public static void main(String[] args) throws Exception {
-		URL oracle = new URL("http://www.oracle.com/");
+		URL oracle = new URL("https://en.wikipedia.org/wiki/Tangent");
 		URLConnection yc = oracle.openConnection();
 		BufferedReader in = new BufferedReader(new InputStreamReader(
 				yc.getInputStream()));
-		PrintWriter fout =  new PrintWriter(new BufferedOutputStream(new FileOutputStream("C:\\Users\\Lakshmipriya\\Desktop\\sample.html")));
-	    	
+		PrintWriter fout = new PrintWriter(new BufferedOutputStream(
+				new FileOutputStream(
+						"C:\\Users\\Lakshmipriya\\Desktop\\sample.html")));
+
 		String inputLine;
-		while ((inputLine = in.readLine()) != null){
+		while ((inputLine = in.readLine()) != null) {
 			String[] words = inputLine.split("\\s+");
 			String newString = "";
-			for(String word: words){
-				if(word.equalsIgnoreCase("the"))
-					newString+= "<FONT style=\"BACKGROUND-COLOR: yellow\"> the </FONT>"+" ";
-				else newString +=word+" ";
+			String find = "Tangent";
+			String replacementString = "";
+			for (String word : words) {
+				if (!word.contains("<Title>") && !word.contains("<title>")) {
+					if (word.contains(find)) {
+						replacementString = "<mark>" + find + "</mark> ";
+						newString += word.replaceAll(find, replacementString);
+						continue;
+					} else if (word.contains(find.toLowerCase())) {
+						replacementString = "<mark> tangent </mark> ";
+						newString += word.replaceAll(find.toLowerCase(),
+								replacementString);
+						continue;
+					} else {
+						replacementString = word + " ";
+					}
+				} else {
+					replacementString = word + " ";
+				}
+				newString+=replacementString;
 			}
-			
+
 			fout.write(newString);
 		}
-			
 		in.close();
-		fout.close();	        
-	        
+		fout.close();
 	}
 }
